eea5b3b2ff934574476414f5220da55e6b81bb83
Sivakar Sithamparanathan <sivakar12@outlook.com>
2022-12-17 14:00:32 +0530

    Render list of project in project page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,48 @@
-export default function Projects() {
+import fs from "fs"
+import matter from "gray-matter"
+
+type Project = {
+  title: string;
+  emoji: string;
+  date: string;
+  summary: string;
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(process.cwd() + "/content/projects")
+  const mdFiles = files.filter((f) => f.endsWith(".md"));
+  // return {
+  //   props: {
+  //     projects: JSON.parse(JSON.stringify(mdFiles.map((f) => { title: f }))),
+  //   }
+  // }
+  const projects = mdFiles.map((f) => {
+    const file = fs.readFileSync(process.cwd() + "/content/projects/" + f);
+    const { data } = matter(file);
+    return data;
+
+  });
+  return {
+    props: {
+      projects: JSON.parse(JSON.stringify(projects)),
+    },
+  };
+}
+export default function Projects({ projects }: { projects: Project[] }) {
+  const projectsOrdered = projects.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+
   return (
     <div>
         <h1>Projects</h1>
         <p>Here are some of my projects</p>
+
+        <ul>
+          {projectsOrdered.map((p) => (
+            <li key={p.title}>{p.emoji} {p.title} - {p.date} </li>
+          ))}
+        </ul>
     </div>
   );
 }
