4a1a274da59cd2c096f2356de4c206de590e41f0
Sivakar Sithamparanathan <sivakar12@outlook.com>
2024-09-06 20:10:31 +0530

    Fix alignment issue in articles page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -6,7 +6,7 @@ export default function ArticlesPage() {
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {articles.map((article) => (
-          <ArticleCard key={article.title} article={article} />
+          <ArticleCard key={article.title} article={article} className="h-full" />
         ))}
       </div>
     </div>
