4503556c9a112ce645b45583a2bab96d9a1d14e4
Sivakar Sithamparanathan <sivakar12@outlook.com>
2025-03-18 17:35:53 +0100

    Change font to Open Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Merriweather } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 
 import "./globals.css";
 import NavigationBar from "@/app/components/NavigationBar";
 
-const merriweather = Merriweather({
+const font = Open_Sans({
   subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-merriweather',
+  weight: ['500', '300', '400', '500', '600'],
+  variable: '--font-open_sans',
 })
 
 export const metadata: Metadata = {
@@ -28,7 +28,7 @@ export default function RootLayout({
     //   </body>
     // </html>
     <html lang="en">
-    <body className={"flex flex-col min-h-screen " + merriweather.className}>
+    <body className={"flex flex-col min-h-screen " + font.className}>
       <nav className="fixed top-0 left-0 right-0 h-16 bg-white shadow z-10">
         <NavigationBar/>
       </nav>
