5ae667df835b498531e658c19940cb469d84814c
Sivakar Sithamparanathan <sivakar12@outlook.com>
2025-04-08 00:33:04 +0200

    Rename "Articles" to "Blog" in Navigation Bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -13,7 +13,7 @@ const navItems: NavItem[] = [
   { name: 'Resume', href: '/resume' },
   { name: 'CS Notes', href: '/cs-notes' },
   { name: 'Projects', href: '/projects' },
-  { name: 'Articles', href: '/articles' },
+  { name: 'Blog', href: '/articles' },
   { name: 'Links', href: '/links' },
 ];
 
