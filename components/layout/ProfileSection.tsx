9ec67576e8b6beb8911fe940433f3df203cf90a1
Sivakar Sithamparanathan <sivakar12@outlook.com>
2025-12-29 23:25:32 +0530

    Fix profile aspect ratio issue

--- components/layout/ProfileSection.tsx
+++ components/layout/ProfileSection.tsx
@@ -11,12 +11,13 @@ export default function ProfileSection({ name, imageSrc, imageAlt }: ProfileSect
     <div className="flex flex-col items-center md:flex-row md:justify-center mb-16 md:space-x-8">
       <div className="rounded-5xl overflow-hidden mb-6 md:mb-0 flex-shrink-0">
         <Image
-          className="h-48 w-48 object-cover animate-card-entrance"
-          style={{ animationDelay: '0ms' }}
+          className="animate-card-entrance"
+          style={{ animationDelay: '0ms', width: 'auto', height: '12rem' }}
           src={imageSrc}
           alt={imageAlt}
-          width={192}
-          height={192}
+          width={0}
+          height={0}
+          sizes="100vw"
         />
       </div>
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center break-words px-4 text-black">
